0e2c1b668af974c5cb71a97c0458c79fcf9a4224
pritish1981 <senapati.pritish@gmail.com>
2024-01-28 10:51:34 +0530

    Update EditDistance.java

--- DynamicProgramming/string/EditDistance.java
+++ DynamicProgramming/string/EditDistance.java
@@ -53,36 +53,67 @@ Explanation 2:
  *
  */
 public class EditDistance {
+	// top-down approach or memoization
+	public static int minDistanceTD(String A, String B) {
+		int n = A.length();
+		int m = B.length();
+		int[][] dp = new int[n + 1][m + 1];
+		for (int[] d : dp)
+			Arrays.fill(d, -1);
+		return minDistanceMemo(A, B, n, m, dp);
+	}
 
-	//Solution 1: Bottom-up approach
-	public static int minDistance(String word1, String word2) {
-		int n = word1.length();
-		int m = word2.length();
+	public static int minDistanceMemo(String A, String B, int n, int m, int[][] dp) {
+		if (n == 0)
+			return m;
+		if (m == 0)
+			return n;
+		if (dp[n][m] != -1) {
+			return dp[n][m];
+		}
+		// if last char is equal in both the strings
+		if (A.charAt(n - 1) == B.charAt(m - 1)) {
+			dp[n][m] = minDistanceMemo(A, B, n - 1, m - 1, dp);
+		} else {// if last char is not equal
+			int insert = minDistanceMemo(A, B, n, m - 1, dp);
+			int delete = minDistanceMemo(A, B, n - 1, m, dp);
+			int replace = minDistanceMemo(A, B, n - 1, m - 1, dp);
+			dp[n][m] = 1 + Math.min(replace, Math.min(insert, delete));
+		}
+		return dp[n][m];
+	}
+	// T.C: O(N*M), S.C: O(N*M)
+
+	// Solution 2: Bottom-up approach or tabulation
+	public static int minDistance(String A, String B) {
+		int n = A.length();
+		int m = B.length();
 		int[][] dp = new int[n + 1][m + 1];
 		// intialize the array
 		for (int i = 0; i <= n; i++)
 			dp[i][0] = i;
-		for (int j = 0; j <= m; j++)											
+		for (int j = 0; j <= m; j++)
 			dp[0][j] = j;
 
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
-				if (word1.charAt(i - 1) == word2.charAt(j - 1))
+				if (A.charAt(i - 1) == B.charAt(j - 1))
 					dp[i][j] = dp[i - 1][j - 1];
 				else {
-					int min = Math.min(dp[i][j - 1], dp[i - 1][j]);
-					dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], min);
+					int insert = dp[i][j-1], delete = dp[i-1][j], replace = dp[i-1][j-1];
+                    dp[i][j] = 1 + Math.min(replace, Math.min(insert, delete));
 				}
 			}
 		}
 		return dp[n][m];
-		//T.C: O(N*M), S.C: O(N*M)
+		// T.C: O(N*M), S.C: O(N*M)
 	}
 
 	public static void main(String[] args) {
-		String A = "abad", B = "abac";
-		System.out.println("Minimum number of steps required to convert A to B :: " + minDistance(A, B));
-		
+		String A = "abcd", B = "abe";
+		System.out.println("Minimum number of steps required to convert A to B using tabulation :: " + minDistance(A, B));
+		System.out.println("Minimum number of steps required to convert A to B using memoization :: " + minDistanceTD(A, B));
+
 	}
 
 }
