ee0663b389167d714d7266243a1f67ada8821e86
pritish1981 <senapati.pritish@gmail.com>
2024-03-11 09:09:32 +0530

    Update MorrisInorderTraversal.java

--- Trees/MorrisInorderTraversal.java
+++ Trees/MorrisInorderTraversal.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
  *
  */
 public class MorrisInorderTraversal {
+	static TreeNode root;
 
 	public static ArrayList<Integer> morrisTraversal(TreeNode root){
 		ArrayList<Integer> ans = new ArrayList<Integer>();
@@ -23,10 +24,10 @@ public class MorrisInorderTraversal {
 				while(temp.right != null && temp.right != curr) {
 					temp = temp.right;
 				}
-				if(temp.right == null) {
+				if(temp.right == null) { // create connection
 					temp.right = curr;
 					curr = curr.left;
-				}else if(temp.right == curr) {
+				}else if(temp.right == curr) { // break the connection
 					temp.right = null;
 					ans.add(curr.val);
 					curr = curr.right;
@@ -35,9 +36,27 @@ public class MorrisInorderTraversal {
 		}
 		return ans;
 	}
+	//T.C: O(n), S.C: O(1)
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Constructed binary tree is
+        1
+      /   \
+     2      3
+   /   \
+  4     5
+ */
+		MorrisInorderTraversal tree = new MorrisInorderTraversal();
+		tree.root = new TreeNode(1);
+		tree.root.left = new TreeNode(2);
+		tree.root.right = new TreeNode(3);
+		tree.root.left.left = new TreeNode(4);
+		tree.root.left.right = new TreeNode(5);
+
+		tree.morrisTraversal(tree.root);
+		System.out.println(tree.morrisTraversal(tree.root));
 
 	}
 
 }
+
+
